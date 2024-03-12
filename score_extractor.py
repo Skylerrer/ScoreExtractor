1b5cda2e6c98441ad1465753058591c1b18af8ea
Mischa Breit <mbreit11@web.de>
2024-03-12 16:02:53 +0100

    Add general TODO list

--- score_extractor.py
+++ score_extractor.py
@@ -1,4 +1,11 @@
 
+
 if __name__ == "__main__":
+
     print("SCORE_EXTRACTOR")
-    
+
+    #TODO
+    # get video file
+    # ask for region of interest
+    # extract score and time from region of interest
+    # save score and time to file (e.g. json / txt / database / ...)
